964015c7dbe058776c74f3a1faca046f2e130227
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2021-12-12 15:12:53 -0300

    Requisitos 10, e 11 + algumas correçoes do Lint

--- script.js
+++ script.js
@@ -1,39 +1,56 @@
 const button = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa');
 const ol = document.querySelector('#lista-tarefas');
+const deleteList = document.querySelector('#apaga-tudo');
+const deleteCompleted = document.querySelector('#remover-finalizados');
+button.innerText = 'Adicionar';
+deleteList.innerText = 'Apaga tudo';
+deleteCompleted.innerText = 'Concluídos';
 
 function inputLength() {
   return input.value.length;
 }
 
 function createList() {
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(input.value));
-    ol.appendChild(li);
-    input.value = '';// para o texto sumir qdo for clicado
-  
-    function changeColor() {
-        li.classList.toggle('change');
-    } 
-    li.addEventListener('click', changeColor);
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(input.value));
+  ol.appendChild(li);
+  input.value = '';
 
-    function lineComplete() {
-        li.classList.toggle('completed');
+  function changeColor() {
+    li.classList.toggle('change');
+  }
+  li.addEventListener('click', changeColor);
+
+  function lineComplete() {
+    li.classList.toggle('completed');
+  }
+  li.addEventListener('dblclick', lineComplete);
+
+  function deleteEventCompleted() {
+    if (li.className === ('completed')) {
+      li.remove('completed');
     }
-    li.addEventListener('dblclick', lineComplete);
-
+  }
+  deleteCompleted.addEventListener('click', deleteEventCompleted);
 }
 
 function addClick() {
-    if (inputLength () > 0){
-        createList();
-}
+  if (inputLength() > 0) {
+    createList();
+  }
 }
 
 function addPress() {
-    if (inputLength () > 0 && event.witch === 13) {
-        createList();
+  if (inputLength() > 0 && event.witch === 13) { // 13 - enter
+    createList();
+  }
 }
-}
+
 button.addEventListener('click', addClick);
 input.addEventListener('keypress', addPress);
+
+function deleteElement() {
+  ol.innerHTML = '';
+}
+deleteList.addEventListener('click', deleteElement);
